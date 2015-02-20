988fa217baf6e329b6a4ffdf730bdd53032bf092
Michael Borkowski <michael@borkowski.at>
2015-02-20 01:46:12 +0100

    (ci) several retries for stable build

--- ci/test.sh
+++ ci/test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+RETRIES=3
 
-cd "$DIR/../scovilleJ"
+function build() {
+	cd "$DIR/../scovilleJ"
+	# -P ci to activate release-like things like javadoc generation (and warnings)
+	mvn -P ci clean verify
+}
 
-# -P ci to activate release-like things like javadoc generation (and warnings)
-mvn -P ci clean verify
+
+
+try=0
+
+while [ $try -lt $RETRIES ]; do
+	build && break
+	((try++))
+done
+
+[ $try -eq 3 ] && exit 1
+exit 0
