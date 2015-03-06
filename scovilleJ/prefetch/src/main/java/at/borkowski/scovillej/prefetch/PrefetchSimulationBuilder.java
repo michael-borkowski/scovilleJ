1f4a59a3e9c0d39b4d01923c5e50b9f23aa7295e
Michael Borkowski <michael@borkowski.at>
2015-03-06 19:02:03 +0100

    feat(pom): module "prefetch"

---
+++ scovilleJ/prefetch/src/main/java/at/borkowski/scovillej/prefetch/PrefetchSimulationBuilder.java
@@ -0,0 +1,11 @@
+package at.borkowski.scovillej.prefetch;
+
+import at.borkowski.scovillej.SimulationBuilder;
+import at.borkowski.scovillej.simulation.Simulation;
+
+public class PrefetchSimulationBuilder {
+   public Simulation create() {
+      SimulationBuilder builder = new SimulationBuilder();
+      
+   }
+}
