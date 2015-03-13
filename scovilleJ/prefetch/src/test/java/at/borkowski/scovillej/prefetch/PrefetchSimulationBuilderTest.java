c847e0b8fbd115516ec1ed01218d5eadf9a8826e
Michael Borkowski <michael@borkowski.at>
2015-03-13 18:33:03 +0100

    test(prefetch): UT for builder, profiling service

---
+++ scovilleJ/prefetch/src/test/java/at/borkowski/scovillej/prefetch/PrefetchSimulationBuilderTest.java
@@ -0,0 +1,93 @@
+package at.borkowski.scovillej.prefetch;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import at.borkowski.scovillej.prefetch.algorithms.PrefetchAlgorithm;
+import at.borkowski.scovillej.simulation.Simulation;
+import at.borkowski.scovillej.simulation.SimulationEvent;
+
+public class PrefetchSimulationBuilderTest {
+
+   PrefetchSimulationBuilder sut;
+
+   PrefetchAlgorithm algorithm;
+
+   HashMap<String, byte[]> files = new HashMap<>();
+   HashMap<Long, Integer> limits = new HashMap<>();
+   Request request0;
+   List<Request> requests = new LinkedList<>();
+   List<Request> allRequests = new LinkedList<>();
+
+   @Before
+   public void setUp() throws Exception {
+      algorithm = mock(PrefetchAlgorithm.class);
+
+      sut = new PrefetchSimulationBuilder();
+
+      limits.put(10L, 100);
+
+      request0 = new Request(1, 2, 3, "file1");
+      requests.add(new Request(4, 5, 6, "file2"));
+      requests.add(new Request(7, 8, 9, "file3"));
+
+      files.put("file1", new byte[10]);
+      files.put("file2", new byte[20]);
+      files.put("file3", new byte[30]);
+
+      allRequests.add(request0);
+      allRequests.addAll(requests);
+   }
+
+   @Test
+   public void test() {
+      sut.algorithm(algorithm);
+      sut.files(files);
+      sut.limit(13);
+      sut.limits(limits);
+      sut.request(request0);
+      sut.requests(requests);
+      sut.totalTicks(10000);
+
+      Simulation simulation = sut.create();
+
+      assertNotNull(sut.getProfiling());
+
+      assertSame(algorithm, sut.test__getFetchClient().getFetchProcessor().getAlgorithm());
+      assertEquals(10, sut.test__getFetchServer().getFileServerProcessor().getFile("file1").length);
+      assertEquals(20, sut.test__getFetchServer().getFileServerProcessor().getFile("file2").length);
+      assertEquals(30, sut.test__getFetchServer().getFileServerProcessor().getFile("file3").length);
+      assertEquals(13, sut.test__getCommunicationService().getService().getUplinkRate(sut.test__getSocketName()).intValue());
+      assertEquals(13, sut.test__getCommunicationService().getService().getDownlinkRate(sut.test__getSocketName()).intValue());
+
+      Collection<SimulationEvent> rateEvents = sut.test__getRateSetter().generateEvents();
+      assertEquals(1, rateEvents.size());
+      SimulationEvent event = rateEvents.iterator().next();
+      assertEquals(10L, event.getScheduledTick());
+      event.executePhase(null);
+      assertEquals(100, sut.test__getCommunicationService().getService().getUplinkRate(sut.test__getSocketName()).intValue());
+      assertEquals(100, sut.test__getCommunicationService().getService().getDownlinkRate(sut.test__getSocketName()).intValue());
+
+      Request[] pendingRequests = sut.test__getFetchClient().getFetchProcessor().getPendingRequests().toArray(new Request[0]);
+      assertEquals(allRequests.size(), pendingRequests.length);
+      for (Request expected : allRequests) {
+         boolean found = false;
+         for (Request r : pendingRequests)
+            if (expected == r)
+               found = true;
+         assertTrue(found);
+      }
+      assertEquals(10000, simulation.getTotalTicks());
+   }
+}
---
+++ scovilleJ/prefetch/src/test/java/at/borkowski/scovillej/prefetch/profiling/PrefetchProfilingServiceImplTest.java
@@ -0,0 +1,113 @@
+package at.borkowski.scovillej.prefetch.profiling;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import at.borkowski.scovillej.impl.series.DoubleSeriesImpl;
+import at.borkowski.scovillej.impl.series.LongSeriesImpl;
+import at.borkowski.scovillej.impl.series.VoidSeriesImpl;
+import at.borkowski.scovillej.prefetch.Request;
+import at.borkowski.scovillej.profile.SeriesProvider;
+import at.borkowski.scovillej.simulation.Simulation;
+import at.borkowski.scovillej.simulation.SimulationContext;
+
+public class PrefetchProfilingServiceImplTest {
+
+   Simulation simulation;
+   SimulationContext context;
+
+   PrefetchProfilingServiceImpl sut;
+
+   Map<String, SeriesProvider<?>> series = new HashMap<>();
+   Map<String, Class<?>> types = new HashMap<>();
+
+   @Before
+   public void setUp() throws Exception {
+      simulation = mock(Simulation.class);
+      context = mock(SimulationContext.class);
+
+      when(context.getSeries(anyString(), (Class<?>) any())).then(handleGetSeriesFromContext());
+      when(simulation.getSeries(anyString(), (Class<?>) any())).then(handleGetSeriesFromContext());
+
+      sut = new PrefetchProfilingServiceImpl();
+      sut.initialize(simulation, context);
+   }
+
+   private Answer<SeriesProvider<?>> handleGetSeriesFromContext() {
+      return new Answer<SeriesProvider<?>>() {
+         @Override
+         public SeriesProvider<?> answer(InvocationOnMock invocation) throws Throwable {
+            String name = (String) invocation.getArguments()[0];
+            Class<?> clazz = (Class<?>) invocation.getArguments()[1];
+
+            if (!series.containsKey(name)) {
+               series.put(name, createSeries(clazz));
+               types.put(name, clazz);
+
+               series.get(name).initialize(simulation);
+            }
+
+            if (clazz.equals(types.get(name)))
+               return series.get(name);
+            else
+               throw new RuntimeException("invalid type: " + clazz + " != " + types.get(name));
+         }
+      };
+   }
+
+   private SeriesProvider<?> createSeries(Class<?> clazz) {
+      if (clazz.equals(Long.class))
+         return new LongSeriesImpl();
+      else if (clazz.equals(Void.class))
+         return new VoidSeriesImpl();
+      else if (clazz.equals(Double.class))
+         return new DoubleSeriesImpl();
+      else
+         throw new RuntimeException("unsupported type: " + clazz);
+   }
+
+   @Test
+   public void testBasicGetters() {
+      assertNull(sut.getPhaseHandlers());
+      assertNull(sut.generateEvents());
+      assertSame(sut, sut.getService());
+      assertEquals(PrefetchProfilingService.class, sut.getServiceClass());
+   }
+
+   @Test
+   public void testFetched() throws Exception {
+      sut.fetched(new Request(1, 2, 3, "file"), 4, 5, 6);
+      assertEquals(1, sut.getURT().getCount());
+      assertEquals(1, sut.getURTperKB().getCount());
+   }
+
+   @Test
+   public void testCacheHit() throws Exception {
+      sut.cacheHit(new Request(1, 2, 3, "file"), 4);
+      assertEquals(1, sut.getCacheHitAges().getCount());
+   }
+
+   @Test
+   public void testCacheMiss() throws Exception {
+      sut.cacheMiss(new Request(1, 2, 3, "file"));
+      assertEquals(1, sut.getCacheMisses().getCount());
+   }
+
+   @Test
+   public void testLateArrival() throws Exception {
+      sut.lateArrival(new Request(1, 2, 3, "file"));
+      assertEquals(1, sut.getOverdue().getCount());
+   }
+
+}
